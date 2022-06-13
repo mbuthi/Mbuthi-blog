860bc4a8e8cb3fc6bf9707071f3cdee46a7a6968
Mbuthi Mungai <mbuthimungai34@gmail.com>
2022-06-13 19:16:49 +0300

    change to database sqlite to postgres

--- main_.py
+++ main_.py
@@ -71,7 +71,7 @@ class User(UserMixin, db.Model, Base):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     posts = relationship("BlogPost", backref="poster")
     comments = relationship("Comment", backref="commenter")
